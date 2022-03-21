f70f8e1bd22e10ee62e2dc2f633b8b20528dd9ce
Jerry Pittman <91224170+jpittma1@users.noreply.github.com>
2022-03-21 06:57:19 -0400

    Update Node.py

--- Node.py
+++ Node.py
@@ -1,31 +1,33 @@
+#!/usr/bin/env python3
+
+#ENPM673 Spring 2022
+#Section 0101
+#Jerry Pittman, Jr. UID: 117707120
 #Maitreya Ravindra Kulkarni, UID: 117506075
-#jpittma1@umd.edu and mkulk98@umd.edu
+#jpittma1@umd.edu and mkulk98@umd.edu 
+#Project #3 Phase 1
+# Node Class Object
 
 import numpy as np
 import math
 
 '''Class of Node to store state, parent, move made, and cost of each move'''
-
 class Node():
-    def __init__(self, state, parent, move, cost): 
+    def __init__(self, state, parent, move, cost):
         # creating tuple with cost to come, index, parent node index=0 and coordinate values (x,y) 
         self.state = state      #index
         self.parent = parent    #parent of current node
         self.move = move
-        self.cost = cost    #cost to come; default will be set to math.inf
+        self.cost = cost   #cost to come; default will be set to math.inf
+
         
-    #---Accessors--
+    #---Accessors---
     def getState(self):
         return self.state
 		
     def getParent(self):
         return self.parent
-
-    def getParentState(self):
-        if self.getParent() is None:
-            return None
-        return self.getParent().getState()
-		
+    
     def getMove(self):
 	    return self.move
 		
@@ -35,20 +37,16 @@ class Node():
     #Override less than operator
     def __lt__(self, other):
         return self.cost < other.cost
+    
+    #Override greater than operator
+    def __gt__(self, other):
+            return self.cost < other.cost
 
     '''Backtracking Function'''
     def getFullPath(self):
         moves = []
         nodes = []
         current_node = self
-        
-        # path_list = []   
-        # parent = Child_parent_map[Goalnode]
-        # path_list.append(Goalnode)
-        
-        # while parent is not None:  # Since the start node doesn’t have any parent
-        #     path_list.append(parent)
-        #     parent = Child_parent_map[parent]
         
         while(current_node.getMove() is not None):
 
